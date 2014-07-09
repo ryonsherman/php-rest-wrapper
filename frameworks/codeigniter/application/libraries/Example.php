553d3a35060c3cee223882a097a4d8b9186f8e03
Ryon Sherman <ryon.sherman@gmail.com>
2014-07-09 10:59:45 -0500

    Provided example CI library

---
+++ frameworks/codeigniter/application/libraries/Example.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once(APPPATH . 'libraries/ci_restwrapper.php');
+
+class Example extends CI_RESTWrapper {}
+
+/* End of file Example.php */
+/* Location: ./application/libraries/Example.php */
